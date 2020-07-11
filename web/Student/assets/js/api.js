0d1e60d76bc64ad8fea7bdaf87e146d18601eb8d
root <root@openols.basicws.net>
2020-07-11 00:59:33 +0000

    stupid

--- web/Student/assets/js/api.js
+++ web/Student/assets/js/api.js
@@ -10,14 +10,14 @@ function getCookie(cname) {
     return "";
 }
 
-function setCourceName(token, userid, courseid) { // This func works but shitty. - mzWyt
+function setCourseName(token, userid, courseid) { // This func works but shitty. - mzWyt
     
     var url = API_URL + '/fetch_course_by_id/?user_id=' + userid + '&token=' + token + '&course_id=' + courseid;
-	var init = API_URL + '/live?user_id=' + userid + '&token=' + token + '&course_id=' + courseid;
-	var html = '<a href="' + init + '">' + data.title + '</a>
+    var init = API_URL + '/live?user_id=' + userid + '&token=' + token + '&course_id=' + courseid;
+    var html = '<a href="' + init + '">'
     $.get(url,function callback(data){
         if (data.status == 'OK'){
-            document.getElementById("last_class").innerHTML = html
+            document.getElementById("last_class").innerHTML = html + data.title + '</a>';
         }
         else{
             alert("获取课程信息失败！");
@@ -60,28 +60,28 @@ function load_mainpage_info() {
             document.getElementById("stu_name").innerHTML = data.information.name;
             document.getElementById("grade_n_sex").innerHTML = data.information.grade + '级学生 | ' + data.information.gender;
 
-            document.getElementById("intro").innerHTML = data.information.intro
+            document.getElementById("intro").innerHTML = data.information.intro;
             document.getElementById("motto").innerHTML = data.information.motto;
             
-            setCourceName(Token, User_Id, data.information.last_course)            
-            document.getElementById("exit_time").innerHTML = data.information.exit_time
+            setCourseName(Token, User_Id, data.information.last_course);   
+            document.getElementById("exit_time").innerHTML = data.information.exit_time;
 
-            var CPU_Usage = data.statistics.Total_Usage.toFixed(1) + "%"
-            var MEM_Total = formatFileSize(data.statistics.Total_Mem)
-            var MEM_Usage = (data.statistics.Total_Mem - data.statistics.Free_Mem )/ data.statistics.Total_Mem * 100
-            MEM_Usage = MEM_Usage.toFixed(1) + "%" //Gemini : Fixed display issue #
+            var CPU_Usage = data.statistics.Total_Usage.toFixed(1) + "%";
+            var MEM_Total = formatFileSize(data.statistics.Total_Mem);
+            var MEM_Usage = (data.statistics.Total_Mem - data.statistics.Free_Mem )/ data.statistics.Total_Mem * 100;
+            MEM_Usage = MEM_Usage.toFixed(1) + "%"; //Gemini : Fixed display issue #
         
-            document.getElementById("pb_CPU_usage").style = "width: " + CPU_Usage + ";"
-            document.getElementById("pb_CPU_usage").innerHTML = CPU_Usage
+            document.getElementById("pb_CPU_usage").style = "width: " + CPU_Usage + ";";
+            document.getElementById("pb_CPU_usage").innerHTML = CPU_Usage;
 
-            document.getElementById("pb_MEM_total").innerHTML = MEM_Total
+            document.getElementById("pb_MEM_total").innerHTML = MEM_Total;
 
-            document.getElementById("pb_MEM_usage").style = "width: " + MEM_Usage + ";"
-            document.getElementById("pb_MEM_usage").innerHTML = MEM_Usage
+            document.getElementById("pb_MEM_usage").style = "width: " + MEM_Usage + ";";
+            document.getElementById("pb_MEM_usage").innerHTML = MEM_Usage;
 
         }
         else{
-            alert("连接服务器失败！")
+            alert("连接服务器失败！");
         }
     });
 
